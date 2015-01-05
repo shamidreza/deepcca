2cda710d215b5fe8f18ac01aba2786f1cf6f673e
shamidreza <hamidreza.plus@gmail.com>
2015-01-05 15:57:24 -0800

    more cleaning the code + adding license info

--- dcca.py
+++ dcca.py
@@ -1,1 +1,30 @@
-s
+"""
+This file is part of deepcca.
+
+deepcca is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+deepcca is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with deepcca.  If not, see <http://www.gnu.org/licenses/>.
+"""
+"""
+
+Deep Canonical Correlation Analysis
+
+References:
+
+[1] G. Andrew, R. Arora, J. Bilmes, and K. Livescu. \
+Deep canonical correlation analysis. In Proc. of\
+the 30th Intl. Conference on Machine Learning, p\
+ages 1247–1255, Atlanta ,Georgia, USA, 2013.
+[2] http://deeplearning.net/
+
+
+"""
--- mlp.py
+++ mlp.py
@@ -1,4 +1,21 @@
 """
+This file is part of deepcca.
+
+deepcca is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+deepcca is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with deepcca.  If not, see <http://www.gnu.org/licenses/>.
+"""
+"""
+
 Multi-layer Perceptron
 
 References:
@@ -7,6 +24,7 @@ http://deeplearning.net/tutorial/mlp.html
 
 
 """
+
 __docformat__ = 'restructedtext en'
 
 
