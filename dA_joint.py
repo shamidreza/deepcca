5a7853a8c2f5a774f26d03b0630b939a19a9a324
shamidreza <hamidreza.plus@gmail.com>
2015-01-25 12:45:04 -0800

    adding regularization

--- dA_joint.py
+++ dA_joint.py
@@ -48,6 +48,10 @@ try:
 except ImportError:
     import Image
 
+try:
+    from matplotlib import pyplot as pp
+except ImportError:
+    print 'matplotlib is could not be imported'
 
 # start-snippet-1
 class dA_joint(object):
@@ -80,6 +84,7 @@ class dA_joint(object):
         theano_rng=None,
         input1=None,
         input2=None,
+        cor_reg=None,
         n_visible1=784/2,
         n_visible2=784/2,
         n_hidden=500,
@@ -231,13 +236,14 @@ class dA_joint(object):
         self.W2_prime = self.W2.T
 
         self.theano_rng = theano_rng
-        #self.L1 = (
-            #abs(self.W*(self.W<0)).sum()
-        #)
+        self.L1 = (
+            abs(self.W1).sum()+abs(self.W2).sum()+abs(self.b1).sum()+abs(self.b2).sum()+abs(self.b1_prime).sum()+abs(self.b2_prime).sum()
+        )
     
-        #self.L2_sqr = (
-            #((self.W**2)*(self.W<0)).sum()
-        #)
+        self.L2_sqr = (
+            (self.W1**2).sum()+(self.W2**2).sum()+abs(self.b1**2).sum()+abs(self.b2**2).sum()+abs(self.b1_prime**2).sum()+abs(self.b2_prime**2).sum()
+
+        )
         # if no input is given, generate a variable representing the input
         if input1 is None:
             # we use a matrix because we expect a minibatch of several
@@ -256,7 +262,10 @@ class dA_joint(object):
         # end-snippet-1
         self.output1 = T.nnet.sigmoid(T.dot(self.x1, self.W1) + self.b1)
         self.output2 = T.nnet.sigmoid(T.dot(self.x2, self.W2) + self.b2)
-        #self.regress
+        self.rec1 = T.nnet.sigmoid(T.dot(self.output1, self.W1_prime) + self.b1_prime)
+        self.rec2 = T.nnet.sigmoid(T.dot(self.output2, self.W2_prime) + self.b2_prime)
+        self.reg = T.nnet.sigmoid(T.dot(self.output1, self.W2_prime) + self.b2_prime)
+        self.cor_reg = theano.shared(numpy.float32(1.0),name='reg')
     def get_corrupted_input(self, input1, input2, corruption_level):
         """This function keeps ``1-corruption_level`` entries of the inputs the
         same and zero-out randomly selected subset of size ``coruption_level``
@@ -318,7 +327,7 @@ class dA_joint(object):
         #L_X1_x2 = - T.sum(y1 * T.log(y2) + (1 - y1) * T.log(1 - y2), axis=1)
         #L_X2_x1 = - T.sum(y2 * T.log(y1) + (1 - y2) * T.log(1 - y1), axis=1)
         L_X1_x2 = T.sum(T.mean((y1-y2)**2,1))
-        cost = T.mean(L_x1) + T.mean(L_x2) + 40.0*T.mean(L_X1_x2)# + 0.2*T.mean(L_X2_x1)
+        cost = T.mean(L_x1) + T.mean(L_x2) + self.cor_reg*T.mean(L_X1_x2)+0.001*self.L1+001*self.L2_sqr# + 0.2*T.mean(L_X2_x1)
 
         # compute the gradients of the cost of the `dA` with respect
         # to its parameters
@@ -330,228 +339,6 @@ class dA_joint(object):
         ]
 
         return (cost, updates)
-
-
-# start-snippet-1
-class dA(object):
-    """Denoising Auto-Encoder class (dA)
-
-    A denoising autoencoders tries to reconstruct the input from a corrupted
-    version of it by projecting it first in a latent space and reprojecting
-    it afterwards back in the input space. Please refer to Vincent et al.,2008
-    for more details. If x is the input then equation (1) computes a partially
-    destroyed version of x by means of a stochastic mapping q_D. Equation (2)
-    computes the projection of the input into the latent space. Equation (3)
-    computes the reconstruction of the input, while equation (4) computes the
-    reconstruction error.
-
-    .. math::
-
-        \tilde{x} ~ q_D(\tilde{x}|x)                                     (1)
-
-        y = s(W \tilde{x} + b)                                           (2)
-
-        x = s(W' y  + b')                                                (3)
-
-        L(x,z) = -sum_{k=1}^d [x_k \log z_k + (1-x_k) \log( 1-z_k)]      (4)
-
-    """
-
-    def __init__(
-        self,
-        numpy_rng,
-        theano_rng=None,
-        input=None,
-        n_visible=784,
-        n_hidden=500,
-        W=None,
-        bhid=None,
-        bvis=None
-    ):
-        """
-        Initialize the dA class by specifying the number of visible units (the
-        dimension d of the input ), the number of hidden units ( the dimension
-        d' of the latent or hidden space ) and the corruption level. The
-        constructor also receives symbolic variables for the input, weights and
-        bias. Such a symbolic variables are useful when, for example the input
-        is the result of some computations, or when weights are shared between
-        the dA and an MLP layer. When dealing with SdAs this always happens,
-        the dA on layer 2 gets as input the output of the dA on layer 1,
-        and the weights of the dA are used in the second stage of training
-        to construct an MLP.
-
-        :type numpy_rng: numpy.random.RandomState
-        :param numpy_rng: number random generator used to generate weights
-
-        :type theano_rng: theano.tensor.shared_randomstreams.RandomStreams
-        :param theano_rng: Theano random generator; if None is given one is
-                     generated based on a seed drawn from `rng`
-
-        :type input: theano.tensor.TensorType
-        :param input: a symbolic description of the input or None for
-                      standalone dA
-
-        :type n_visible: int
-        :param n_visible: number of visible units
-
-        :type n_hidden: int
-        :param n_hidden:  number of hidden units
-
-        :type W: theano.tensor.TensorType
-        :param W: Theano variable pointing to a set of weights that should be
-                  shared belong the dA and another architecture; if dA should
-                  be standalone set this to None
-
-        :type bhid: theano.tensor.TensorType
-        :param bhid: Theano variable pointing to a set of biases values (for
-                     hidden units) that should be shared belong dA and another
-                     architecture; if dA should be standalone set this to None
-
-        :type bvis: theano.tensor.TensorType
-        :param bvis: Theano variable pointing to a set of biases values (for
-                     visible units) that should be shared belong dA and another
-                     architecture; if dA should be standalone set this to None
-
-
-        """
-        self.n_visible = n_visible
-        self.n_hidden = n_hidden
-
-        # create a Theano random generator that gives symbolic random values
-        if not theano_rng:
-            theano_rng = RandomStreams(numpy_rng.randint(2 ** 30))
-
-        # note : W' was written as `W_prime` and b' as `b_prime`
-        if not W:
-            # W is initialized with `initial_W` which is uniformely sampled
-            # from -4*sqrt(6./(n_visible+n_hidden)) and
-            # 4*sqrt(6./(n_hidden+n_visible))the output of uniform if
-            # converted using asarray to dtype
-            # theano.config.floatX so that the code is runable on GPU
-            initial_W = numpy.asarray(
-                numpy_rng.uniform(
-                    low=-4 * numpy.sqrt(6. / (n_hidden + n_visible)),
-                    high=4 * numpy.sqrt(6. / (n_hidden + n_visible)),
-                    size=(n_visible, n_hidden)
-                ),
-                dtype=theano.config.floatX
-            )
-            W = theano.shared(value=initial_W, name='W', borrow=True)
-
-        if not bvis:
-            bvis = theano.shared(
-                value=numpy.zeros(
-                    n_visible,
-                    dtype=theano.config.floatX
-                ),
-                borrow=True
-            )
-
-        if not bhid:
-            bhid = theano.shared(
-                value=numpy.zeros(
-                    n_hidden,
-                    dtype=theano.config.floatX
-                ),
-                name='b',
-                borrow=True
-            )
-
-        self.W = W
-        # b corresponds to the bias of the hidden
-        self.b = bhid
-        # b_prime corresponds to the bias of the visible
-        self.b_prime = bvis
-        # tied weights, therefore W_prime is W transpose
-        self.W_prime = self.W.T
-        self.theano_rng = theano_rng
-        self.L1 = (
-            abs(self.W*(self.W<0)).sum()
-        )
-    
-        self.L2_sqr = (
-            ((self.W**2)*(self.W<0)).sum()
-        )
-        # if no input is given, generate a variable representing the input
-        if input is None:
-            # we use a matrix because we expect a minibatch of several
-            # examples, each example being a row
-            self.x = T.dmatrix(name='input')
-        else:
-            self.x = input
-
-        self.params = [self.W, self.b, self.b_prime]
-    # end-snippet-1
-
-    def get_corrupted_input(self, input, corruption_level):
-        """This function keeps ``1-corruption_level`` entries of the inputs the
-        same and zero-out randomly selected subset of size ``coruption_level``
-        Note : first argument of theano.rng.binomial is the shape(size) of
-               random numbers that it should produce
-               second argument is the number of trials
-               third argument is the probability of success of any trial
-
-                this will produce an array of 0s and 1s where 1 has a
-                probability of 1 - ``corruption_level`` and 0 with
-                ``corruption_level``
-
-                The binomial function return int64 data type by
-                default.  int64 multiplicated by the input
-                type(floatX) always return float64.  To keep all data
-                in floatX when floatX is float32, we set the dtype of
-                the binomial to floatX. As in our case the value of
-                the binomial is always 0 or 1, this don't change the
-                result. This is needed to allow the gpu to work
-                correctly as it only support float32 for now.
-
-        """
-        return self.theano_rng.binomial(size=input.shape, n=1,
-                                        p=1 - corruption_level,
-                                        dtype=theano.config.floatX) * input
-
-    def get_hidden_values(self, input):
-        """ Computes the values of the hidden layer """
-        return T.nnet.sigmoid(T.dot(input, self.W) + self.b)
-
-    def get_reconstructed_input(self, hidden):
-        """Computes the reconstructed input given the values of the
-        hidden layer
-
-        """
-        return T.nnet.sigmoid(T.dot(hidden, self.W_prime) + self.b_prime)
-
-    def get_cost_updates(self, corruption_level, learning_rate):
-        """ This function computes the cost and the updates for one trainng
-        step of the dA """
-
-        tilde_x = self.get_corrupted_input(self.x, corruption_level)
-        y = self.get_hidden_values(tilde_x)
-        z = self.get_reconstructed_input(y)
-        # note : we sum over the size of a datapoint; if we are using
-        #        minibatches, L will be a vector, with one entry per
-        #        example in minibatch
-        L = - T.sum(self.x * T.log(z) + (1 - self.x) * T.log(1 - z), axis=1)
-        # note : L is now a vector, where each element is the
-        #        cross-entropy cost of the reconstruction of the
-        #        corresponding example of the minibatch. We need to
-        #        compute the average of all these to get the cost of
-        #        the minibatch
-        
-        L1_reg = 0.00
-        L2_reg = 0.00
-        cost = T.mean(L) + L1_reg * self.L1 + L2_reg * self.L2_sqr
-        
-        # compute the gradients of the cost of the `dA` with respect
-        # to its parameters
-        gparams = T.grad(cost, self.params)
-        # generate the list of updates
-        updates = [
-            (param, param - learning_rate * gparam)
-            for param, gparam in zip(self.params, gparams)
-        ]
-
-        return (cost, updates)
-
 
 
 def test_dA(learning_rate=0.1, training_epochs=150,
@@ -586,7 +373,7 @@ def test_dA(learning_rate=0.1, training_epochs=150,
     index = T.lscalar()    # index to a [mini]batch
     x1 = T.matrix('x1')  # the data is presented as rasterized images
     x2 = T.matrix('x2')  # the data is presented as rasterized images
-
+    cor_reg = T.scalar('cor_reg')
     if not os.path.isdir(output_folder):
         os.makedirs(output_folder)
     os.chdir(output_folder)
@@ -613,10 +400,10 @@ def test_dA(learning_rate=0.1, training_epochs=150,
         theano_rng=theano_rng,
         input1=x1,
         input2=x2,
+        cor_reg=cor_reg,
 
         n_visible1=28 * 28/2,
         n_visible2=28 * 28/2,
-
         n_hidden=50
     )
 
@@ -624,7 +411,7 @@ def test_dA(learning_rate=0.1, training_epochs=150,
         corruption_level=0.3,
         learning_rate=learning_rate
     )
-
+    cor_reg_val = numpy.float32(1.0)
     train_da = theano.function(
         [index],
         cost,
@@ -632,7 +419,6 @@ def test_dA(learning_rate=0.1, training_epochs=150,
         givens={
             x1: train_set_x[index * batch_size: (index + 1) * batch_size],
             x2: train_set_y[index * batch_size: (index + 1) * batch_size]
-
         }
     )
     fprop_x1 = theano.function(
@@ -651,6 +437,22 @@ def test_dA(learning_rate=0.1, training_epochs=150,
                },
                name='fprop_x2'
     )
+    fprop_x1_to_x2 = theano.function(
+               [],
+               outputs=da.reg,
+               givens={
+                   x1: test_set_x
+               },
+               name='fprop_x12x2'
+    )
+    updates_reg = [
+            (da.cor_reg, da.cor_reg+theano.shared(numpy.float32(1.0)))
+    ]
+    update_reg = theano.function(
+        [],
+        updates=updates_reg
+    )
+
     
     start_time = time.clock()
 
@@ -664,10 +466,14 @@ def test_dA(learning_rate=0.1, training_epochs=150,
         c = []
         for batch_index in xrange(n_train_batches):
             c.append(train_da(batch_index))
-
+        
+        #cor_reg_val += 1
+        #da.cor_reg = theano.shared(cor_reg_val) 
+        update_reg()
         print 'Training epoch %d, cost ' % epoch, numpy.mean(c)
         H1=fprop_x1()
         H2=fprop_x2()
+        X2H=fprop_x1_to_x2()
         print 'Training epoch %d, cost ' % epoch, cor_cost(H1, H2)
     end_time = time.clock()
 
@@ -687,65 +493,7 @@ def test_dA(learning_rate=0.1, training_epochs=150,
     
     print cor
         
-    #####################################
-    # BUILDING THE MODEL CORRUPTION 30% #
-    #####################################
-
-    rng = numpy.random.RandomState(123)
-    theano_rng = RandomStreams(rng.randint(2 ** 30))
-
-    da = dA(
-        numpy_rng=rng,
-        theano_rng=theano_rng,
-        input=x,
-        n_visible=28 * 28,
-        n_hidden=500
-    )
-
-    cost, updates = da.get_cost_updates(
-        corruption_level=0.3,
-        learning_rate=learning_rate
-    )
-
-    train_da = theano.function(
-        [index],
-        cost,
-        updates=updates,
-        givens={
-            x: train_set_x[index * batch_size: (index + 1) * batch_size]
-        }
-    )
-
-    start_time = time.clock()
-
-    ############
-    # TRAINING #
-    ############
-
-    # go through training epochs
-    for epoch in xrange(training_epochs):
-        # go through trainng set
-        c = []
-        for batch_index in xrange(n_train_batches):
-            c.append(train_da(batch_index))
-
-        print 'Training epoch %d, cost ' % epoch, numpy.mean(c)
-
-    end_time = time.clock()
-
-    training_time = (end_time - start_time)
-
-    print >> sys.stderr, ('The 30% corruption code for file ' +
-                          os.path.split(__file__)[1] +
-                          ' ran for %.2fm' % (training_time / 60.))
-
-    image = Image.fromarray(tile_raster_images(
-        X=da.W.get_value(borrow=True).T,
-        img_shape=(28, 28), tile_shape=(10, 10),
-        tile_spacing=(1, 1)))
-    image.save('filters_corruption_30.png')
-
-    os.chdir('../')
+  
 
 
 if __name__ == '__main__':
